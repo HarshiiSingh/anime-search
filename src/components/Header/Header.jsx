210db26aee44d6dfc153a1956859166877b012d4
Harshit Singh <harshi6196@gmail.com>
2025-03-12 01:38:33 -0400

    added randomAnime button and functionality, user is now able to select multiple genres to find a random anime. Header and all components need to be stylized

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
-import { useState} from 'react'
+import { useState, useEffect } from 'react'
 import "./Header.css";
-import { animeBySearch } from "../../utils/jikanapi";
+import { animeBySearch, getGenres, getRandomAnime, getRandomAnimeByGenre } from "../../utils/jikanapi";
 function Header() {
   const [search, setSearch] = useState("");
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState([]);
+
+  useEffect(() => {
+    getGenres().then(setGenres); // Fetch genres when component mounts
+}, []);
+
   const handleSearch = (e) => {
     if (e.key === "Enter" && search.trim()) {
       console.log("Search for: ", search);
@@ -14,6 +21,37 @@ function Header() {
         .catch((error) => console.log(error));
     }
   };
+
+  const handleGenreChange = (e) => {
+    const selectedValues = Array.from(e.target.selectedOptions, option => option.value);
+    setSelectedGenre(selectedValues);
+};
+
+  const handleRandomAnime = () => {
+    if (selectedGenre.length > 0) {
+        console.log(`Fetching a random anime from genres: ${selectedGenre.join(", ")}`);
+        getRandomAnimeByGenre(selectedGenre)
+        .then(randomAnime => {
+            if (randomAnime) {
+                console.log("Random anime from Selected Genres: ", randomAnime);
+            } else {
+                console.log("No anime found for these genres.");
+            }
+        })
+    } else {
+        console.log("Fetching a random anime");
+        getRandomAnime()
+        .then(randomAnime => {
+            if (randomAnime) {
+                console.log("Random anime: ", randomAnime);
+            } else {
+                console.log("No anime found");
+            }
+        })
+        .catch(error => console.error("Error fetching random anime:", error));
+    }
+
+  }
   return (
     <>
       {" "}
@@ -28,6 +66,14 @@ function Header() {
             onChange={(e) => setSearch(e.target.value)}
             onKeyDown={handleSearch}
           />
+          <select multiple value={selectedGenre} onChange={handleGenreChange} className="header__genre" id="">
+            {genres.map(genre => (
+                <option key={genre.mal_id} value={genre.mal_id}>
+                    {genre.name}
+                </option>
+            ))}
+          </select>
+          <button type="button" onClick={handleRandomAnime} className="header__randomAnimeBtn">Random Anime</button>
         </div>
       </header>
     </>
